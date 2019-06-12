5e90cf742587c39c4ff7847dc20edd9a0c47ba13
CyderN <1023778740@qq.com>
2019-06-12 21:28:02 +0800

    add class

---
+++ src/localization.cpp
@@ -0,0 +1,12 @@
+//
+// Created by xcy on 19-6-12.
+//
+#include "tree_tracker.h"
+int main(int argc, char** argv) {
+    google::InitGoogleLogging(argv[0]);
+    ros::init(argc, argv, "tree_tracker");
+    tree_tracker my_tree_tracker;
+    ros::spin();
+    return 0;
+}
+
