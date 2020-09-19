aaf540e2fc1c5ab51ecafdb55e0e93c69f0b1a97
CyderN <1023778740@qq.com>
2020-09-19 16:07:46 +0800

    add cuda structure

---
+++ src/dbscan_cuda.cuh
@@ -0,0 +1,52 @@
+//
+// Created by xcy on 2020/9/15.
+//
+
+#ifndef SRC_DBSCAN_CUDA_CUH
+#define SRC_DBSCAN_CUDA_CUH
+
+#include <ros/ros.h>
+#include <iostream>
+#include <sensor_msgs/PointCloud.h>
+#include <ctime>
+
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+using namespace std;
+
+class cudaDbscanDriver{
+private:
+    double EPS ;
+    int MinPts ;
+    void clusterVector(vector<vector<double>> currentVector, vector<int> currentIndex,
+                       double currentCluster, sensor_msgs::PointCloud &PCL);
+    void ellipseSearch(vector<vector<double>>& currentResult, vector<int>& currentIndex,
+                       geometry_msgs::Point32 queryPts);
+    vector<double> P2stdV(geometry_msgs::Point32 point){
+        vector<double> vector;
+        vector.resize(3);
+        vector[0] = point.x;
+        vector[1] = point.y;
+        vector[2] = point.z;
+        return vector;
+    }
+    geometry_msgs::Point32 stdV2P(vector<double> vector){
+        geometry_msgs::Point32 point;
+        point.x = vector[0];
+        point.y = vector[1];
+        point.z = vector[2];
+        return point;
+    }
+public:
+    void dbscanClustering(sensor_msgs::PointCloud& PCL);
+    sensor_msgs::PointCloud PCLforOutput;
+    void setEPSandMinPts(double eps, int minpts){EPS = eps; MinPts = minpts;}
+
+    enum {visited = 0 , type = 1, cluster = 2};
+    enum {core = 0, border = 1, noise = 2, little = 3, strange = 4, pass = 5};
+
+};
+
+
+#endif //SRC_DBSCAN_CUDA_CUH
